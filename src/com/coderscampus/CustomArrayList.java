c711b227c932cd5eb6457d657e5bd6315c7946c5
Moe0088 <you@example.com>
2024-07-01 11:23:10 -0400

    a3

--- src/com/coderscampus/CustomArrayList.java
+++ src/com/coderscampus/CustomArrayList.java
@@ -1,24 +1,35 @@
 package com.coderscampus;
 
+import java.util.Arrays;
+
 public class CustomArrayList<T> implements CustomList<T> {
+
 	Object[] items = new Object[10];
+	int size = 0;
 
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
-		return false;
+		if (size == items.length) {
+			items = Arrays.copyOf(items, items.length * 2);
+		}
+		items[size] = item;
+		size++;
+
+		return true;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return size;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return (T) items[index];
+
 	}
 
 }
